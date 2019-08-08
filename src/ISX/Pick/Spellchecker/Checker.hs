895dfdf1985be645a96209b0375e179370bcf51c
tiredpixel <tiredpixel@posteo.de>
2019-08-08 12:41:07 +0000

    Checker: implement, with tests, supporting en-gb only

---
+++ src/ISX/Pick/Spellchecker/Checker.hs
@@ -0,0 +1,92 @@
+module ISX.Pick.Spellchecker.Checker (
+    Dict(..),
+    Result(..),
+    check
+    ) where
+
+
+import              System.Process                          (readProcess)
+import qualified    Data.Text                               as  T
+
+
+data Dict =
+    DictEn |
+    DictEnGB
+
+data Result =
+    ResultOk |
+    ResultRoot WordRoot |
+    ResultCompound |
+    ResultMiss WordOriginal WordOffset [WordMiss] |
+    ResultNone WordOriginal WordOffset
+    deriving (Show, Eq)
+
+type Results = [(Text, [Result])]
+
+type WordMiss = Text
+
+type WordOffset = Integer
+
+type WordOriginal = Text
+
+type WordRoot = Text
+
+check :: [Dict] -> [Text] -> IO Results
+check dicts texts = parse texts <$> hunspell dicts' texts
+    where
+        dicts' = if null dicts
+            then [dictDef]
+            else dicts
+
+
+dictDef :: Dict
+dictDef = DictEn
+
+hunspell :: [Dict] -> [Text] -> IO [Text]
+hunspell dicts texts = drop 1 . lines . toText <$> readProcess "hunspell" [
+    "-d", toString $ T.intercalate "," dicts',
+    "-a"
+    ] (toString $ unlines texts')
+    where
+        -- ! terse-mode; ^ disable prefix [SEC]; replace inner newlines [SEC]
+        texts' = "!" : (("^" <>) . T.replace "\n" " " <$> texts)
+        dicts' = hunspellDict <$> dicts
+
+hunspellDict :: Dict -> Text
+hunspellDict d = case d of
+    DictEn   -> "en_GB"
+    DictEnGB -> "en_GB"
+
+parse :: [Text] -> [Text] -> Results
+parse texts results = zip texts results'
+    where
+        results' = (parseLine <$>) . filter ("" /=) <$>
+            (lines <$> T.splitOn "\n\n" (unlines results))
+
+parseLine :: Text -> Result
+parseLine l = fromMaybe (error "invalid spellchecker output") $ case ctrl of
+    "*" -> Just ResultOk
+    "+" -> Just $ ResultRoot lRem
+    "-" -> Just ResultCompound
+    "&" -> parseLineMiss lRem
+    "#" -> parseLineNone lRem
+    _ -> Nothing
+    where
+        (ctrl, lRem_) = T.breakOn " " l
+        lRem = T.drop 1 lRem_
+
+parseLineMiss :: Text -> Maybe Result
+parseLineMiss lRem = do
+    o' <- readMaybe $ toString o
+    return $ ResultMiss w o' misses'
+    where
+        (meta, misses_) = T.breakOn ":" lRem
+        [w, _, o] = words meta -- discard misses count
+        misses' = T.splitOn ", " $ T.drop 2 misses_
+
+parseLineNone :: Text -> Maybe Result
+parseLineNone lRem = do
+    o' <- readMaybe $ toString o
+    return $ ResultNone w o'
+    where
+        (w, o) = T.breakOn " " lRem
---
+++ test/ISX/Pick/Spellchecker/CheckerSpec.hs
@@ -0,0 +1,127 @@
+module ISX.Pick.Spellchecker.CheckerSpec (spec) where
+
+
+import              ISX.Pick.Spellchecker.Checker
+import              ISX.Test
+import              Prelude                                 hiding  (get)
+import              Relude.Unsafe                           ((!!))
+
+
+{-# ANN module ("HLint: ignore Reduce duplication" :: String) #-}
+
+
+spec :: Spec
+spec =
+    -- mistakes added and formatting changed purposely
+    describe "check" $ do
+        
+        describe "default" $ do
+            -- https://www.theguardian.com/world/2019/aug/08/the-sustainable-surfer-meet-the-man-behind-the-worlds-first-fully-recyclable-wetsuit
+            
+            let check' = check []
+            
+            it "ok empty" $ do
+                rs <- check' []
+                rs `shouldBe` []
+            
+            it "ok word" $ do
+                let ls = [
+                        "biodegradable"]
+                rs <- check' ls
+                rs `shouldBe` [
+                    (ls !! 0, [])]
+            
+            it "ok sentence" $ do
+                let ls = [
+                        "Out the top of St Agnes, a steep village on Cornwall’s north coast, sits a small utopia."]
+                rs <- check' ls
+                rs `shouldBe` [
+                    (ls !! 0, [])]
+            
+            it "ok paragraphs" $ do
+                let ls = [
+                        "“We could have waited for one of the bigger wetsuit brands to figure this all out, but we didn’t, which I think is testament to how dedicated we are to our ethos of looking after our environment and bringing about positive change,” says Banks.",
+                        "At any rate: the joy of surfing lies in never tiring of the pursuit of perfection, so their temperaments are well suited to their mission."]
+                rs <- check' ls
+                rs `shouldBe` [
+                    (ls !! 0, []),
+                    (ls !! 1, [])]
+            
+            it "miss typo" $ do
+                let ls = [
+                        "Out the top of St Agnes, a steeep village on Cornwall’s north coast, sits a small utopia."]
+                rs <- check' ls
+                rs `shouldBe` [
+                    (ls !! 0, [
+                        ResultMiss "steeep" 28 ["steep","steppe","testee","step","teepee"]])]
+            
+            it "miss multiline" $ do
+                let ls = [
+                        "There is stil a lot more testing\nto go – next, Kay and other testers will wear the suit with tempurature sensors",
+                        "for a more precize asessment of tHe heat situation."]
+                rs <- check' ls
+                rs `shouldBe` [
+                    (ls !! 0, [
+                        ResultMiss "stil" 10 ["slit","stile","stilt","still","silt","sail","stir","soil","instil","pistil","distil"],
+                        ResultMiss "tempurature" 94 ["temperature","tempura","premature","maturate"]]),
+                    (ls !! 1, [
+                        ResultMiss "precize" 12 ["precise","prize"],
+                        ResultMiss "asessment" 20 ["assessment","amassment","easement","assent"],
+                        ResultMiss "tHe" 33 ["t He","the","tee","tie","toe","he","thee","then","them","they","she","thy"]])]
+            
+            it "miss control" $ do
+                let ls = [
+                        "*steeep"]
+                rs <- check' ls
+                rs `shouldBe` [
+                    (ls !! 0, [
+                        ResultMiss "steeep" 2 ["steep","steppe","testee","step","teepee"]])]
+            
+            it "miss strange" $ do
+                let ls = [
+                        "aslkdflaasdfsdfhf"]
+                rs <- check' ls
+                rs `shouldBe` [
+                    (ls !! 0, [
+                        ResultNone "aslkdflaasdfsdfhf" 1])]
+        
+        describe "en" $ do
+            -- https://www.theguardian.com/environment/2019/aug/08/climate-crisis-reducing-lands-ability-to-sustain-humanity-says-ipcc
+            
+            let check' = check [DictEn]
+            
+            it "ok sentence" $ do
+                let ls = [
+                        "Global heating is increasing droughts, soil erosion and wildfires while diminishing crop yields in the tropics and thawing permafrost near the poles, says the report by the Intergovernmental Panel on Climate Change."]
+                rs <- check' ls
+                rs `shouldBe` [
+                    (ls !! 0, [])]
+            
+            it "miss sentence" $ do
+                let ls = [
+                        "GloBal heeting is increesing droughts, soil erosion and wildfires whyle diminishing crop yields in the tropics and thawing permafrost near the Poles, says the report by the Intergovernmental Panel on Climate Change."]
+                rs <- check' ls
+                rs `shouldBe` [
+                    (ls !! 0, [
+                        ResultMiss "GloBal" 1 ["Global","Glob al","Glob-al"],
+                        ResultMiss "heeting" 8 ["sheeting","heating","heeling","heeding","meeting","hefting","greeting"],
+                        ResultMiss "increesing" 19 ["increasing","screening","resining","cresting","resisting"],
+                        ResultMiss "whyle" 67 ["while","whale","whole","why le","why-le","Whaley"]])]
+        
+        describe "en-gb" $ do
+            let check' = check [DictEnGB]
+            
+            it "ok en-gb" $ do
+                let ls = [
+                        "colours"]
+                rs <- check' ls
+                rs `shouldBe` [
+                    (ls !! 0, [])]
+            
+            it "miss en-us" $ do
+                let ls = [
+                        "colors"]
+                rs <- check' ls
+                rs `shouldBe` [
+                    (ls !! 0, [
+                        ResultMiss "colors" 1 ["colours","colons","col ors","col-ors","Coors","Corso"]])]
